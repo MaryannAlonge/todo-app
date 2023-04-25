42f70e7c25846b8d0b28983f78d7afb2e6e500dc
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-04-25 01:29:44 +0100

    middle ware

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-const express = require("express")
+const express = require("express");
+const cors = require("cors")
 
 // run the express libary using app
 const app = express();
@@ -7,7 +8,11 @@ app.listen(5000, () => {
   console.log("server running on port 5000")
 }); //test by running node index on terminal
 
-// install nodemon to watch your file instead of node
+// install nodemon to watch your file instead of node(so run nodemon index after install)
 
 
-// create middlware
+// create middlware(anytime we need to create a middlware we use app.use)
+app.use(cors());
+
+// we must handle data from the client side using express.json(this gives us access to request.body)
+app.use(express.json());
