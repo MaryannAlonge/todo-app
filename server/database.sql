39d698d0046f825ff88e5f5764ef9bb8cfc60500
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-04-25 23:01:16 +0100

    still database

--- server/database.sql
+++ server/database.sql
@@ -3,5 +3,6 @@ CREATE DATABASE perntodo;
 
 -- create table for todos and specify the schema and data types
 CREATE TABLE todo(
-  todo_id SERIAL PRIMARY KEY
+  todo_id SERIAL PRIMARY KEY, 
+  description VARCHAR(255)
 )
