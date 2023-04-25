7c0774e2bd5fe78c2628831951a52e4696cd1764
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-04-25 23:51:46 +0100

    get all  todo route and query

--- server/index.js
+++ server/index.js
@@ -44,7 +44,15 @@ app.post("/todos", async(req, res) => {
 });
 
 // get all todos
-
+app.get("/todos", async(req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message)
+    
+  }
+})
 // get a todo
 
 //edit/update a todo
