f0da696c6d966ab37a5b94e38a840ff921f210f7
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-04-28 19:06:29 +0100

    complete add todo

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function InputTodo() {
+ const  InputTodo =() =>{
+
+  const [description, setDescription] = useState("");
+
+
+  // handle form submission
+  const onSubmitForm = async e => {
+    e.preventDefault();
+    try {
+      // send a request to the api
+      const body = {description};
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        //stringify the data coming from the body
+        body: JSON.stringify(body)
+      });
+
+      console.log(response)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+
   return (
     <>
     <h1 className='text-center mt-5'>My Todo List</h1>
-    <form className='d-flex'>
-      <input type="text" className='form-control'/>
+    <form className='d-flex mt-5' onSubmit={onSubmitForm}>
+      {/* set and change the state value */}
+      <input type="text" className='form-control' 
+      value={description} 
+      onChange={e => setDescription(e.target.value)}
+      />
       <button className='btn btn-success'>Add</button>
     </form>
     </>
   )
 }
+
+export default  InputTodo;;
