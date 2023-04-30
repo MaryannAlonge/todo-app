f8524f57110d1202f150a3225eadcdcf28e934cc
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-04-30 02:40:46 +0100

    finish up

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -27,14 +27,18 @@ import React, {useState} from 'react'
 </button>
 
 {/* <!-- The Modal --> */}
-<div className="modal" id={`id${todo.todo_id}`}>
+<div className="modal" 
+id={`id${todo.todo_id}`}
+onClick={() => setDescription(todo.description)}
+>
   <div className="modal-dialog">
     <div className="modal-content">
 
       {/* <!-- Modal Header --> */}
       <div className="modal-header">
         <h4 className="modal-title">Edit Todo</h4>
-        <button type="button" className="close" data-dismiss="modal">&times;</button>
+        <button type="button" className="close" data-dismiss="modal" onClick={() => setDescription(todo.description)}>
+          &times;</button>
       </div>
 
       {/* <!-- Modal body --> */}
@@ -54,7 +58,7 @@ import React, {useState} from 'react'
         >
           Edit
           </button>
-        <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-danger" data-dismiss="modal"  onClick={() => setDescription(todo.description)}>Close</button>
       </div>
 
     </div>
